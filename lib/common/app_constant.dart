8ff7a0a3c90a043475c221c49c8e1aa2ec2e7b1b
phamtanphat <phatdroid94@gmail.com>
2023-03-16 21:00:43 +0700

    singleton dio

---
+++ lib/common/app_constant.dart
@@ -0,0 +1,7 @@
+class AppConstant {
+  static const String BASE_URL = "http://api.openweathermap.org/";
+  static const int CONNECTION_TIME_OUT = 30;
+  static const int RECEIVE_TIME_OUT = 30;
+  static const int SEND_TIME_OUT = 30;
+  static const int WRITE_TIME_OUT = 30;
+}
---
+++ lib/data/api/dio_client.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_weather_forcast/common/app_constant.dart';
+
+class DioClient {
+  static final Dio _dio = DioClient._createDio();
+
+  DioClient._();
+
+  static Dio getDio() {
+    return _dio;
+  }
+
+  static Dio _createDio() {
+    return Dio(BaseOptions(
+      baseUrl: AppConstant.BASE_URL,
+      connectTimeout: Duration(seconds: AppConstant.CONNECTION_TIME_OUT),
+      receiveTimeout: Duration(seconds: AppConstant.RECEIVE_TIME_OUT),
+      sendTimeout: Duration(seconds: AppConstant.SEND_TIME_OUT),
+    ));
+  }
+}
